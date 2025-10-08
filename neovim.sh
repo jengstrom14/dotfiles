5a02d5400c7621c6c4c85e3054e3649c0347841b
Jackson Engstrom <engstromjackson@gmail.com>
2025-10-08 16:11:36 -0700

    changes neovim depth and update script

--- neovim.sh
+++ neovim.sh
@@ -1,8 +1,8 @@
 sudo apt update && sudo apt install -y git ninja-build gettext \
     cmake unzip curl build-essential
 
-git clone https://github.com/neovim/neovim
+git clone --branch stable --depth 1 https://github.com/neovim/neovim
 cd neovim
-git checkout stable
+#git checkout stable
 make CMAKE_BUILD_TYPE=RelWithDebInfo
 sudo make install
