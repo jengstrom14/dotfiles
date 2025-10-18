22ee26c467161d023bc6dff58f0db3de76720598
Jackson Engstrom <mail@jacksonengstrom.com>
2025-10-17 19:12:56 -0700

    adds mako config file

--- katara/omarchy/install.sh
+++ katara/omarchy/install.sh
@@ -5,12 +5,14 @@ DIR="$HOME/.config"
 if test -d "$DIR"; then
     mv $DIR/hypr $DIR/hypr.dtbak
     mv $DIR/waybar $DIR/waybar.dtbak
-    mv $DIR/pipewire/ $DIR/pipewire.dtbak
+    mv $DIR/pipewire $DIR/pipewire.dtbak
+    mv $DIR/mako/ $DIR/mako.dtbak
 else
     mkdir $DIR
 fi
 ln -s $PWD/hypr $DIR/hypr
 ln -s $PWD/waybar $DIR/waybar
 ln -s $PWD/pipewire $DIR/pipewire
+ln -s $PWD/mako $DIR/mako
 
 echo "Installed"
