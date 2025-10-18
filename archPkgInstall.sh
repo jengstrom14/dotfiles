f87952a0d08ce0352c96a4e3f5e8c27c8a6db61f
Jackson Engstrom <mail@jacksonengstrom.com>
2025-10-17 18:10:00 -0700

    fixes arch install script to not need confirmation

--- archPkgInstall.sh
+++ archPkgInstall.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo pacman -S --needed base-devel git rust
+sudo pacman -S --needed base-devel git rust --noconfirm
 git clone https://aur.archlinux.org/paru.git
 cd paru
-makepkg -si
+makepkg -si --noconfirm
 
 paru -S ripgrep noise-suppression-for-voice-git netbird-bin zen-browser-bin --noconfirm
 
