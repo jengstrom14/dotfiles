bcf1c09898e3c4371ecc6e2d6b3f8b00cf98a0ae
Jackson Engstrom <mail@jacksonengstrom.com>
2025-10-24 09:05:49 -0700

    replaces wget with wcurl

--- horizon-temp-fix.sh
+++ horizon-temp-fix.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /tmp
-wget https://archive.archlinux.org/packages/l/libxkbcommon/libxkbcommon-1.11.0-1-x86_64.pkg.tar.zst
-wget https://archive.archlinux.org/packages/l/libxkbcommon-x11/libxkbcommon-x11-1.11.0-1-x86_64.pkg.tar.zst
+wcurl https://archive.archlinux.org/packages/l/libxkbcommon/libxkbcommon-1.11.0-1-x86_64.pkg.tar.zst
+wcurl https://archive.archlinux.org/packages/l/libxkbcommon-x11/libxkbcommon-x11-1.11.0-1-x86_64.pkg.tar.zst
 
 sudo pacman -U libxkbcommon-1.11.0-1-x86_64.pkg.tar.zst libxkbcommon-x11-1.11.0-1-x86_64.pkg.tar.zst
