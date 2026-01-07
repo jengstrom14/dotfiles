4c1075ad542741568aa4359f134ca4d8ac4bffdc
Jackson Engstrom <engstromjackson@gmail.com>
2026-01-07 09:16:46 -0800

    updates telescope and bumps nvim packages

--- nvim/lua/jackson/lazy.lua
+++ nvim/lua/jackson/lazy.lua
@@ -14,8 +14,12 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
 	{ 
 		"nvim-telescope/telescope.nvim", 
-		tag = '0.1.6',
-		dependencies = { "nvim-lua/plenary.nvim" },
+		tag = 'v0.2.1',
+		dependencies = 
+        {
+            'nvim-lua/plenary.nvim',
+            { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+        },
 		config = function()
 			local builtin = require('telescope.builtin')
 			vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
